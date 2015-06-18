cc0b7a7e5f19422c6c2635013aed9c068de9d9d5
brettkan <bkfighton@gmail.com>
2015-06-18 11:02:56 -0700

    (feat) add database config file

--- server/db/config.js
+++ server/db/config.js
@@ -1,58 +1,64 @@
-// var Bookshelf = require('bookshelf');
-// var knex = require('knex');
-// var path = require('path');
+var Bookshelf = require('bookshelf');
+var knex = require('knex');
+var path = require('path');
 
-// var knex = require('knex')({
-//   client: 'sqlite3',
-//   connection: {
-//     host: '127.0.0.1',
-//     user: 'your_database_user',
-//     password: 'password',
-//     database: 'FFDrafter',
-//     charset: 'utf8',
-//     filename: path.join(__dirname, '../db/FFDrafter.sqlite')
-//   }
-// });
+var knex = require('knex')({
+  client: 'mysql',
+  connection: {
+    host: '127.0.0.1',
+    user: 'user',
+    password: 'password',
+    database: 'howWasIt',
+    charset: 'utf8',
+    filename: path.join(__dirname, './howWasIt.sql')
+  }
+});
 
-// var db = require('bookshelf')(knex);
+var db = require('bookshelf')(knex);
 
-// db.knex.schema.hasTable('users').then(function(exists) {
-//   if (!exists) {
-//     db.knex.schema.createTable('users', function (user) {
-//       user.increments('id').primary();
-//       user.string('username', 30);
-//       user.timestamps();
-//     }).then(function (table) {
-//       console.log('Created Table', table);
-//     });
-//   }
-// });
+db.knex.schema.hasTable('users').then(function(exists) {
+  if (!exists) {
+    db.knex.schema.createTable('users', function (user) {
+      user.increments('id').primary();
+      user.string('username', 30);
+      user.string('email', 50);
+      user.string('first_name', 30);
+      user.string('last_name', 30);
+      user.string('password', 30);
+      user.timestamps();
+    }).then(function (table) {
+      console.log('Created Table', table);
+    });
+  }
+});
 
-// db.knex.schema.hasTable('players').then(function(exists) {
-//   if (!exists) {
-//     db.knex.schema.createTable('players', function (player) {
-//       player.increments('id').primary();
-//       player.string('first_name', 30);
-//       player.string('last_name', 30);
-//       player.string('position', 30);
-//       player.string('team', 30);      
-//     }).then(function (table) {
-//       console.log('Created Table', table);
-//     });
-//   }
-// });
+db.knex.schema.hasTable('reviews').then(function(exists) {
+  if (!exists) {
+    db.knex.schema.createTable('reviews', function (review) {
+      review.increments('id').primary();
+      review.integer('user_id', 20).references('users.id');
+      review.string('review_text', 255);
+      review.integer('up_votes', 10);
+      review.integer('google_loc_id', 30);
+      review.string('google_loc_name', 30);
+      review.string('google_loc_coordinates', 30);
+      review.timestamps();
+    }).then(function (table) {
+      console.log('Created Table', table);
+    });
+  }
+});
 
-// db.knex.schema.hasTable('players_users').then(function(exists) {
-//   if (!exists) {
-//     db.knex.schema.createTable('players_users', function (player_user) {
-//       player_user.integer('user_id').references('users.id');
-//       player_user.integer('player_id').references('players.id');
-//       player_user.integer('round');
-//       player_user.timestamps();      
-//     }).then(function (table) {
-//       console.log('Created Table', table);
-//     });
-//   }
-// });
+db.knex.schema.hasTable('user_connections').then(function(exists) {
+  if (!exists) {
+    db.knex.schema.createTable('user_connections', function (user_connection) {
+      user_connection.integer('user_id').references('users.id');
+      user_connection.integer('friend_user_id').references('users.id');
+      user_connection.timestamps();
+    }).then(function (table) {
+      console.log('Created Table', table);
+    });
+  }
+});
 
-// module.exports = db;
+module.exports = db;
