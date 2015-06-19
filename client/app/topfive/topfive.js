3c027df0e232bc3fb1c4470c1bfe8079e740eeb0
Josh <Josh@Joshs-MacBook-Pro.local>
2015-06-18 17:25:51 -0700

    add angular factory and controller for topfive list

---
+++ client/app/topfive/topfive.js
@@ -0,0 +1,35 @@
+angular.module('howWasIt.topfive', [])
+
+.factory('Reviews', function ($http){
+
+	var getAll = function(){
+		return http$({
+			method: 'GET',
+			url: //path to db
+		});
+		.then(function(res){
+			return res.data;
+		});
+	};
+
+	return {
+      getAll: getAll
+    };
+});
+
+
+.controller('TopFiveController', function($scope, Reviews){
+
+	$scope.data = {};
+
+	$scope.getReviews = function () {
+		Reviews.getAll()
+		  .then(function(reviews){
+		  	$scope.data.reviews = reviews.body;
+		  })
+		  .catch(function (error){
+		  	console.log
+		  })
+	};
+	
+});
