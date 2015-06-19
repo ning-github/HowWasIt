a75392bf00bb60ea3bf3987dea38f286f7d98580
brettkan <bkfighton@gmail.com>
2015-06-19 11:48:20 -0700

    (bug) fix typo in servers/routes.js

--- server/routes.js
+++ server/routes.js
@@ -1,7 +1,7 @@
 var controllers = require("./controllers");
 var router = require("express").Router();
 
-for (var route in controlelrs) {
+for (var route in controllers) {
 	router.route("/" + route)
 		.get(controllers[route].get)
 		.post(controllers[route].post);
