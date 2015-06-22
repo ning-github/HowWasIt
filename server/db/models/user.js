574ae982ea310e16904a74739265c48c1a6527dd
Josh <Josh@Joshs-MacBook-Pro.local>
2015-06-22 15:56:28 -0700

    (feat) partially finished authentication

--- server/db/models/user.js
+++ server/db/models/user.js
@@ -1,6 +1,7 @@
 var db = require('../config');
 var Review = require('./review.js');
 var UserConnection = require('./userConnection.js');
+var bcrypt = require('bcrypt');
 
 var User = db.Model.extend({
   tableName: 'users',
@@ -11,6 +12,17 @@ var User = db.Model.extend({
 
   friends: function() {
     return this.belongsToMany(User, 'user_connections', 'user_id', "friend_user_id");
+  },
+
+  validPassword: function(password){
+    bcrypt.hash(password, null, null, function(err, hash){
+      bcrypt.compare(password, hash, function(err, res){
+        if (err){
+          console.log("Error checking password: ", err);
+        }
+        return res;
+      });
+    });
   }
 
 });
