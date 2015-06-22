48ea2f00846512cb22934c227f384b9299f100a0
Josh <Josh@Joshs-MacBook-Pro.local>
2015-06-22 15:44:50 -0700

    (feat) update package.json for deployment.

--- server/myServer.js
+++ server/myServer.js
@@ -1,22 +1,23 @@
 var express = require("express");
-
 var morgan = require("morgan");
 var parser = require("body-parser");
-
 var router = require("./routes.js");
 
 var db = require('./db/config');
 
+
 var app = express();
 module.exports.app = app;
 
-app.set("port", 3000);
+app.set("port", process.env.PORT || 3000);
 
 app.use(morgan("dev"));
 app.use(parser.json());
 
 app.use("/", router);
 
+app.use(passport.initialize());
+app.use(passport.session());
 //TODO: Need to hookup the client here!
 app.use(express.static(__dirname + "/../client"));
 
