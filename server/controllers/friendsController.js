9db8fb34c3314f2957407932648f17ad76b7bb91
brettkan <bkfighton@gmail.com>
2015-06-22 13:29:36 -0700

    (feat) add searchMembers function in friendsController on back-end

--- server/controllers/friendsController.js
+++ server/controllers/friendsController.js
@@ -13,13 +13,12 @@ module.exports = {
 
   getFriendList: {
     get: function (req, res) {
-      console.log('GETFRIENDLIST FUNCTION HAPPENED, url requested: ', req.url);
-
       var userId = url.parse(req.url).query.split('=')[1]; // url format: /friends/getFriendList?user_id=123
       
       User.forge().where({id: userId}).fetch({
         withRelated: ['friends']
       }).then(function(model) {
+        // see user database model 
         res.status(200).send(model.related('friends').models);
       });
 
@@ -33,11 +32,20 @@ module.exports = {
   searchMembers: {
     get: function (req, res) {
       console.log('SEARCHMEMBERS FUNCTION HAPPENED, url requested: ', req.url);
-      // db.Message.findAll({include: [db.User]})
-      //   .complete(function(err, results){
-      //     // optional mapping step
-      //     res.json(results);
-      //   });
+      
+      var query = url.parse(req.url).query.split('=')[1]; // url format: /friends/getFriendList?query=123
+
+      // Search all substrings of username, first_name, last_name, and email fields
+      User.forge().query(function(qb) {
+        qb.where('username', 'LIKE', '%' + query + '%')
+        .orWhere('first_name', 'LIKE', '%' + query + '%')
+        .orWhere('last_name', 'LIKE', '%' + query + '%')
+        .orWhere('email', 'LIKE', '%' + query + '%');
+      })
+      .fetchAll().then(function(models) {
+        res.status(200).send(models.models);
+      });
+      
     },
     post: function (req, res) {
       // TODO: Fix, for some reason routes.js needs this here.
@@ -76,8 +84,6 @@ module.exports = {
       res.status(400).send('Bad Request');
     },
     post: function (req, res) {
-      console.log('REMOVEFRIEND FUNCTION HAPPENED, url requested: ', req.url);
-
       var userId = url.parse(req.url).query.split('=')[1]; // url format: /friends/getFriendList?user_id=123
       var friendId = req.body.id;
 
@@ -87,7 +93,6 @@ module.exports = {
           res.status(200).send('User is not your friend!');
         } else {
           found.where({user_id: userId, friend_user_id: friendId}).destroy().then(function(connection) {
-            console.log(connection);
             UserConnections.remove(connection);
             // Send back friend ID so that client can remove friend from friendList
             res.status(200).send({friendId: friendId});
