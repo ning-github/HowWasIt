aa2814a7d09e789d06eee87859cac0a1706952d0
brettkan <bkfighton@gmail.com>
2015-06-23 13:21:23 -0700

    (refactor) change Authentication service

--- client/app/services/services.js
+++ client/app/services/services.js
@@ -1,17 +1,24 @@
 angular.module('howWasIt.services', [])
 
-.service('checkLoggedIn', function($http, $q, $state){
+.service('Authentication', function($http, $q, $state){
   
-  var deferred = $q.defer();
+  var checkLoggedIn = function() {  
+    var deferred = $q.defer();
 
-  $http.get('/loggedIn').success(function(user){
+    $http.get('/loggedIn').success(function(user){
 
-    if (user){
-      deferred.resolve();
-    } else {
-      console.log("You need to log in.");
-      deferred.reject();
-      $state.go('login');
-    }
-  })
-})
+      if (user){
+        deferred.resolve();
+      } else {
+        console.log("You need to log in.");
+        deferred.reject();
+        $state.go('login');
+      }
+    });
+  };
+
+  return {
+    checkLoggedIn: checkLoggedIn
+  };
+
+});
