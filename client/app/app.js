7baf1cd3978cc0c947d47d752f25ff044bf6bcd2
brettkan <bkfighton@gmail.com>
2015-06-22 21:30:29 -0700

    (refactor) move map, friends, top five into home controller

--- client/app/app.js
+++ client/app/app.js
@@ -3,4 +3,13 @@ angular.module('howWasIt', [
   'howWasIt.friends',
   'howWasIt.topfive',
   'ui.router'
-]);
+])
+.config(function($stateProvider, $urlRouterProvider) {
+  $urlRouterProvider.otherwise("/home");
+  $stateProvider
+    .state('home', {
+      url:"/home",
+      templateUrl: 'app/home/home.html',
+      controller: 'HomeController'
+    });
+});
